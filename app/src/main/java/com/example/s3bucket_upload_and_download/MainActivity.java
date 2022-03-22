4f42ccf5f70c16a61e0804dad94d6e2668e9211f
C0012S <qkrtkd1002@duksung.ac.kr>
2022-03-23 01:59:02 +0900

    Android Studio에서 S3 Bucket으로 파일 Upload

---
+++ app/src/main/java/com/example/s3bucket_upload_and_download/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.s3bucket_upload_and_download;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferNetworkLossHandler;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+//    String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/img"; // 외부 저장소 경로 → 오류 발생
+    String path = "/data/data/com.example.s3bucket_upload_and_download/img"; // 내부 저장소 경로
+//    File file = new File(path);
+    File file = new File(path + "/FennecFox.jpg");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("외부 저장소 경로 : ", Environment.getExternalStorageDirectory().getAbsolutePath());
+//                uploadWithTransferUtilty(file.getName(), file);
+                uploadWithTransferUtilty("FennecFox", file);
+            }
+        });
+    }
+
+    public void uploadWithTransferUtilty(String fileName, File file) {
+
+        AWSCredentials awsCredentials = new BasicAWSCredentials("access_Key", "secret_Key");    // IAM User의 (accessKey, secretKey)
+        AmazonS3Client s3Client = new AmazonS3Client(awsCredentials, Region.getRegion(Regions.US_EAST_1));
+
+        TransferUtility transferUtility = TransferUtility.builder().s3Client(s3Client).context(this.getApplicationContext()).build();
+        TransferNetworkLossHandler.getInstance(this.getApplicationContext());
+
+        TransferObserver uploadObserver = transferUtility.upload("bucket_Name", fileName, file);    // (bucket name, file 이름, file 객체)
+
+        uploadObserver.setTransferListener(new TransferListener() {
+            @Override
+            public void onStateChanged(int id, TransferState state) {
+                if (state == TransferState.COMPLETED) {
+                    // Handle a completed upload
+                    Log.d("S3 Bucket ", "Upload Completed!");
+                }
+            }
+
+            @Override
+            public void onProgressChanged(int id, long current, long total) {
+                int done = (int) (((double) current / total) * 100.0);
+                Log.d("MYTAG", "UPLOAD - - ID: $id, percent done = $done");
+            }
+
+            @Override
+            public void onError(int id, Exception ex) {
+                Log.d("MYTAG", "UPLOAD ERROR - - ID: $id - - EX:" + ex.toString());
+            }
+        });
+    }
+}
