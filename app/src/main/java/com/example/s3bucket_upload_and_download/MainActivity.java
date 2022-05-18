8f92f5064b0a34239d7bd2fc21afa002f16bd352
C0012S <alwaysh1002@gmail.com>
2022-05-18 11:03:38 +0900

    Android Studio에서 S3 Bucket 안의 파일 Download

--- app/src/main/java/com/example/s3bucket_upload_and_download/MainActivity.java
+++ app/src/main/java/com/example/s3bucket_upload_and_download/MainActivity.java
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
 //    File file = new File(path);
     File file = new File(path + "/FennecFox.jpg");
 
+    File downloadFile = new File(path + "/FennecFox2.jpg");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +42,15 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("외부 저장소 경로 : ", Environment.getExternalStorageDirectory().getAbsolutePath());
 //                uploadWithTransferUtilty(file.getName(), file);
                 uploadWithTransferUtilty("FennecFox", file);
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Button : ", "S3 Bucket Download Button Click!");
+                downloadWithTransferUtilty("FennecFox2.jpg", downloadFile);
             }
         });
     }
@@ -72,6 +83,38 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onError(int id, Exception ex) {
                 Log.d("MYTAG", "UPLOAD ERROR - - ID: $id - - EX:" + ex.toString());
+            }
+        });
+    }
+
+    public void downloadWithTransferUtilty(String fileName, File file) {
+        AWSCredentials awsCredentials = new BasicAWSCredentials("access_Key", "secret_Key");    // IAM User의 (accessKey, secretKey)
+        AmazonS3Client s3Client = new AmazonS3Client(awsCredentials, Region.getRegion(Regions.US_EAST_1));
+
+        TransferUtility transferUtility = TransferUtility.builder().s3Client(s3Client).context(this.getApplicationContext()).build();
+        TransferNetworkLossHandler.getInstance(this.getApplicationContext());
+
+//        TransferObserver downloadObserver = transferUtility.download("bucket_Name", fileName, file);    // (bucket name, file 이름, file 객체)
+        TransferObserver downloadObserver = transferUtility.download("bucket_Name/folder_Name", fileName, file);    // (bucket name/folder name, file 이름, file 객체)    // Bucket 안의 Folder에 있는 파일을 다운로드
+
+        downloadObserver.setTransferListener(new TransferListener() {
+            @Override
+            public void onStateChanged(int id, TransferState state) {
+                if (state == TransferState.COMPLETED) {
+                    // Handle a completed download
+                    Log.d("S3 Bucket ", "Download Completed!");
+                }
+            }
+
+            @Override
+            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                int done = (int) (((double) bytesCurrent / bytesTotal) * 100.0);
+                Log.d("MYTAG", "DOWNLOAD - - ID: " + id + " percent done = " + done);
+            }
+
+            @Override
+            public void onError(int id, Exception ex) {
+                Log.d("MYTAG", "DOWNLOAD ERROR - - ID: " + id + " - - EX: " + ex.toString());
             }
         });
     }
